9e78ff1d78a16ca58933a0e3e8b56330302571a7
Benjamin Agic <benj769b@edu.easj.dk>
2018-11-05 11:03:55 +0100

    s

--- src/js/index.ts
+++ src/js/index.ts
@@ -19,7 +19,7 @@ let user:HTMLInputElement=<HTMLInputElement>document.getElementById("Username");
 let pas:HTMLInputElement=<HTMLInputElement>document.getElementById("password");
 //As it says when you hoover with your mouse over it,when window loads (something) that is the first thing user sees
 window.onload=()=>{
-    let uri:string="https://loginusersapi.azurewebsites.net/api/userslogin";
+    let uri:string="https://birthawebservice20181031094923.azurewebsites.net/api/user";
     axios.get<IUsers[]>(uri)
     .then(function(response:AxiosResponse<IUsers[]>):void{
 response.data.forEach((user:IUsers)=>{
