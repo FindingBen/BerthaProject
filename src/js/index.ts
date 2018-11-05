58bcf3e279797b691975d268fc86e199ac109b98
Issa <abdu2997@edu.easj.dk>
2018-11-05 11:15:59 +0100

    111111111

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,48 +4,51 @@ import axios, {
 }
     from "../../node_modules/axios/index";
 
-    interface IUsers{
+interface IUsers {
 
-        name:string;
-        email:string;
-        password:string;
-    }
+    name: string;
+    email: string;
+    password: string;
+
+
+}
 //Here is the list of users we have in database,by this list we are calling users from database
-let userList:Array<IUsers>=[];
+let userList: Array<IUsers> = [];
 
-let log:HTMLButtonElement=<HTMLButtonElement>document.getElementById("Logs");
-log.addEventListener("click",Login);
+let log: HTMLButtonElement = <HTMLButtonElement>document.getElementById("Logs");
+log.addEventListener("click", Login);
 
-let user:HTMLInputElement=<HTMLInputElement>document.getElementById("Username");
-let pas:HTMLInputElement=<HTMLInputElement>document.getElementById("password");
+let user: HTMLInputElement = <HTMLInputElement>document.getElementById("Username");
+let pas: HTMLInputElement = <HTMLInputElement>document.getElementById("password");
 //As it says when you hoover with your mouse over it,when window loads (something) that is the first thing user sees
-window.onload=()=>{
+window.onload = () => {
 
-    let uri:string="https://birthawebservice20181031094923.azurewebsites.net/api/user";
+    let uri: string = "https://birthawebservice20181031094923.azurewebsites.net/api/user";
     axios.get<IUsers[]>(uri)
-    .then(function(response:AxiosResponse<IUsers[]>):void{
-response.data.forEach((user:IUsers)=>{
-    if(user!=null){
-        userList.push(user);
-    }
-})
-console.log(userList);
-    })
-    .catch(function(error:AxiosError){
-console.log(error);
-    });
+        .then(function (response: AxiosResponse<IUsers[]>): void {
+            response.data.forEach((user: IUsers) => {
+                if (user != null) {
+                    userList.push(user);
+                }
+            })
+            console.log(userList);
+        })
+        .catch(function (error: AxiosError) {
+            console.log(error);
+        });
 }
 
 //Logic behind Login
-function Login():void{
+function Login(): void {
 
-    let inputUsername: string=user.value;
-    let inputPassword: string=pas.value;
+    let inputUsername: string = user.value;
+    let inputPassword: string = pas.value;
+    
     userList.forEach(element => {
-        if(inputUsername==element.name&&inputPassword==element.password){
+        if (inputUsername == element.name && inputPassword == element.password) {
             window.open('MainPage.htm');
         }
-        else{
+        else {
             alert("Wrong credentials!")
         }
     });
---
+++ src/js/userRegistration.ts
@@ -0,0 +1,1 @@
+//implement Post for users reg
